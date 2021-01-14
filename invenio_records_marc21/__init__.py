b463ced3307db39c446d9e3a4c02388b03b30a50
Philipp Gualdi <philipp.gualdi@tugraz.at>
2021-01-14 16:15:48 +0100

    refactor: D400 error
    adding a "." at the end of string

--- invenio_records_marc21/__init__.py
+++ invenio_records_marc21/__init__.py
@@ -5,8 +5,7 @@
 # Invenio-Records-Marc21 is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-
-"""Marc21 datamodel"""
+"""Marc21 datamodel."""
 
 from __future__ import absolute_import, print_function
 
