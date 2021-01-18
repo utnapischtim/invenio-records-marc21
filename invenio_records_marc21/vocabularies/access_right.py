e2ac0eb841c39b806f6cc7f571fc322061a3f2e8
Philipp Gualdi <philipp.gualdi@tugraz.at>
2021-01-18 16:17:08 +0100

    feature(internalapi): provide vocabulary
    providing a vocabulary for the access rights to marc21 records.
    Describe privileges to access records.

---
+++ invenio_records_marc21/vocabularies/access_right.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Graz University of Technology.
+#
+# Invenio-Records-Marc21 is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+
+"""Contributor Role Vocabulary."""
+
+from .vocabulary import Vocabulary
+
+
+class AccessRightVocabulary(Vocabulary):
+    """Encapsulates all access right choices.
+
+    NOTE: Perhaps a stretch of Vocabulary, but very useful.
+    """
+
+    key_field = "access_right"
+
+    @property
+    def readable_key(self):
+        """Returns the key to readable values for this vocabulary."""
+        return "access_right_name"
+
+    @property
+    def vocabulary_name(self):
+        """Returns the human readable name for this vocabulary."""
+        return "access right"
+
+    def get_entry_by_dict(self, dict_key):
+        """Returns a vocabulary entry as an OrderedDict."""
+        if isinstance(dict_key, str):
+            dict_key = {self.key_field: dict_key}
+        return super().get_entry_by_dict(dict_key)
