0703b5cd02936ab5e5100213a845d2d7f7035928
Philipp Gualdi <philipp.gualdi@tugraz.at>
2021-02-26 13:38:25 +0100

    build: update dependencies
    removing unused idutils as dependency and update min dependenciy,
    due to build errors and update invenio-drafts-resources,
    where resource-record version > 10.2. and SchemaLink is added

--- setup.py
+++ setup.py
@@ -53,10 +53,9 @@ setup_requires = [
 install_requires = [
     "invenio-i18n>=1.3.0",
     "dojson>=1.4.0",
-    "idutils>=1.1.7",
-    "invenio-records-rest>=1.4.0,<2.0.0",
-    "invenio-drafts-resources>=0.7.2,<0.10.0",
-    "invenio-vocabularies>=0.1.6,<1.0.0",
+    "invenio-records-rest>=1.5.0,<2.0.0",
+    "invenio-drafts-resources>=0.9.7,<0.10.0",
+    "invenio-vocabularies>=0.3.3,<0.4.0",
 ]
 
 packages = find_packages()
