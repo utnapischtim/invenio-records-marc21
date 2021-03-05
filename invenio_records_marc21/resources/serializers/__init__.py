146c1582f7a4252617ac773668c06ec5ccc5f02f
Philipp Gualdi <philipp.gualdi@tugraz.at>
2021-03-05 14:44:55 +0100

    refactor: module structure
    upating  the module structure. By moving the records specific compontents into records folder.
    The serializer components into resources.
    
    PS: Marshmallow and loader has been removed, since we dont use them right now.

--- invenio_records_marc21/resources/serializers/__init__.py
+++ invenio_records_marc21/resources/serializers/__init__.py
@@ -5,13 +5,8 @@
 # Invenio-Records-Marc21 is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Schemas for marshmallow."""
+"""Record serializers."""
 
 from __future__ import absolute_import, print_function
 
-from .json import MetadataSchemaV1, RecordSchemaV1
-
-__all__ = (
-    "MetadataSchemaV1",
-    "RecordSchemaV1",
-)
+__all__ = ()
