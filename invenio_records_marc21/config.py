fb830914ce0199309e9b041234e23626e95c2508
Gualdi, Philipp <philipp.gualdi@tugraz.at>
2021-03-11 15:32:00 +0100

    refactor: module config
    updating module config for records_marc21 and prepare record
    landingpage ui config and module config keys starting with MARC21_*

--- invenio_records_marc21/config.py
+++ invenio_records_marc21/config.py
@@ -9,26 +9,20 @@
 
 from __future__ import absolute_import, print_function
 
-from invenio_indexer.api import RecordIndexer
-from invenio_records_rest.utils import allow_all, check_elasticsearch
-from invenio_search import RecordsSearch
+MARC21_BASE_TEMPLATE = "invenio_records_marc21/base.html"
 
-RECORDS_REST_ENDPOINTS = {}
+MARC21_REST_ENDPOINTS = {}
 """REST API for invenio-records-marc21."""
 
-RECORDS_UI_ENDPOINTS = {
-    "marcid": {
-        "pid_type": "marcid",
-        "route": "/marc21/<pid_value>",
-        "template": "invenio_records_marc21/record.html",
-    },
+MARC21_UI_ENDPOINTS = {
+    "index": "/marc21/",
+    "record_search": "/marc21/search",
+    "record_detail": "/marc21/<pid_value>",
 }
 """Records UI for invenio-records-marc21."""
 
 SEARCH_UI_JSTEMPLATE_RESULTS = "templates/invenio_records_marc21/results.html"
 """Result list template."""
 
-PIDSTORE_RECID_FIELD = "marcid"
-
-INVENIO_RECORDS_MARC21_ENDPOINTS_ENABLED = True
+MARC21_ENDPOINTS_ENABLED = True
 """Enable/disable automatic endpoint registration."""
