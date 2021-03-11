9de0c82c67719eaee7071c4d2d1de967e981c6fd
Gualdi, Philipp <philipp.gualdi@tugraz.at>
2021-03-11 15:07:48 +0100

    feature: current record proxy
    adding current_records_marc21 local proxy to module to simplify UI handling

---
+++ invenio_records_marc21/proxies.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Graz University of Technology.
+#
+# Invenio-Records-Marc21 is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Helper proxy to the state object."""
+
+from flask import current_app
+from werkzeug.local import LocalProxy
+
+current_records_marc21 = LocalProxy(
+    lambda: current_app.extensions["invenio-records-marc21"]
+)
+"""Helper proxy to get the current records marc21 extension."""
