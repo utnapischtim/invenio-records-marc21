7f1b0c134ba45d2a26afcaf8333658657b337a64
Gualdi, Philipp <philipp.gualdi@tugraz.at>
2021-08-11 14:43:36 +0200

    modification: AccessSchema
    removing embardo validation since a protection class  added
    to the marc21 record and add field_name to ValidationError if
    protection value for metadata and files is invalid

--- invenio_records_marc21/services/schemas/access/parent.py
+++ invenio_records_marc21/services/schemas/access/parent.py
@@ -7,7 +7,7 @@
 # Invenio-Records-Marc21 is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
-"""Marc21 parent record access schemas."""
+"""Marc21 record ParentAccessSchema."""
 
 from flask_babelex import lazy_gettext as _
 from marshmallow import Schema, ValidationError, fields, validates_schema
@@ -26,14 +26,3 @@ class ParentAccessSchema(Schema):
     """Access schema."""
 
     owned_by = List(fields.Nested(Agent))
-
-    @validates_schema
-    def validate_embargo(self, data, **kwargs):
-        """Validate that the properties are consistent with each other."""
-        metadata = data.get("metadata", "")
-        embargo = data.get("embargo", None)
-        if AccessStatusEnum.EMBARGOED.value == metadata and not embargo:
-            raise ValidationError(
-                _("Embargo schema must be set if metadata is Embargoed"),
-                field_name="embargo",
-            )
