d58ebf7ac82c9d69ec0cb267c22424d55551c8f6
Gualdi, Philipp <philipp.gualdi@tugraz.at>
2021-10-28 07:55:08 +0200

    bugfix: init files service in registry

--- setup.py
+++ setup.py
@@ -102,6 +102,7 @@ setup(
         ],
         "invenio_base.blueprints": [
             "invenio_records_marc21_ui = invenio_records_marc21.ui:create_blueprint",
+            "invenio_records_marc21_ext = invenio_records_marc21.views:blueprint",
         ],
         "invenio_db.models": [
             "invenio_records_marc21_model = invenio_records_marc21.records.models",
