aaf6b4140b0d1e31f60c09e70bbbb6ef47ed792d
Philipp Gualdi <philipp.gualdi@tugraz.at>
2022-02-01 13:16:54 +0100

    feature: pids tasks register external pid

---
+++ invenio_records_marc21/services/pids/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+#
+# Copyright (C) 2021 Graz University of Technology.
+#
+# Invenio-Records-Marc21 is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""PIDs module."""
+
+from .tasks import register_or_update_pid
+
+__all__ = ("register_or_update_pid",)
