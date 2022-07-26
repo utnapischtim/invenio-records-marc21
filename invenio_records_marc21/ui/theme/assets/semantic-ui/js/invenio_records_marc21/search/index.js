fa93d6ba37a38317739c8a8196aa0e6cba94f796
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-07-26 13:25:37 +0200

    use search react components provided by invenio-app-rdm
    with this step only the necessary components have to be written by the
    invenio-records-marc21 packages. in the future also those should be used from
    invenio-app-rdm
    
    further use the multiple version of SearchBar.element to have the posibility to
    search in different areas.

--- invenio_records_marc21/ui/theme/assets/semantic-ui/js/invenio_records_marc21/search/index.js
+++ invenio_records_marc21/ui/theme/assets/semantic-ui/js/invenio_records_marc21/search/index.js
@@ -6,28 +6,29 @@
 // modify it under the terms of the MIT License; see LICENSE file for more
 // details.
 
-
 import { createSearchAppInit } from "@js/invenio_search_ui";
 import {
-  Marc21BucketAggregationElement,
-  Marc21RecordFacets,
-  Marc21RecordFacetsValues,
+  RDMBucketAggregationElement,
+  RDMRecordFacets,
+  RDMRecordFacetsValues,
+  RDMRecordSearchBarContainer,
+  RDMRecordMultipleSearchBarElement,
+  RDMToggleComponent,
+  RDMCountComponent,
+} from "@js/invenio_app_rdm/search/components";
+import {
   Marc21RecordResultsGridItem,
   Marc21RecordResultsListItem,
-  Marc21RecordSearchBarContainer,
-  Marc21RecordSearchBarElement,
-  Marc21ToggleComponent,
-  Marc21CountComponent,
 } from "./components";
 
 const initSearchApp = createSearchAppInit({
-  "BucketAggregation.element": Marc21BucketAggregationElement,
-  "BucketAggregationValues.element": Marc21RecordFacetsValues,
+  "BucketAggregation.element": RDMBucketAggregationElement,
+  "BucketAggregationValues.element": RDMRecordFacetsValues,
   "ResultsGrid.item": Marc21RecordResultsGridItem,
   "ResultsList.item": Marc21RecordResultsListItem,
-  "SearchApp.facets": Marc21RecordFacets,
-  "SearchApp.searchbarContainer": Marc21RecordSearchBarContainer,
-  "SearchBar.element": Marc21RecordSearchBarElement,
-  "SearchFilters.ToggleComponent": Marc21ToggleComponent,
-  "Count.element": Marc21CountComponent,
+  "SearchApp.facets": RDMRecordFacets,
+  "SearchApp.searchbarContainer": RDMRecordSearchBarContainer,
+  "SearchBar.element": RDMRecordMultipleSearchBarElement,
+  "SearchFilters.ToggleComponent": RDMToggleComponent,
+  "Count.element": RDMCountComponent,
 });
