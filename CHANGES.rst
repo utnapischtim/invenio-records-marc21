3227ded3eb89fdf2266d7df739249429abc05c09
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-08-01 13:28:41 +0200

    release v0.6.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -8,6 +8,11 @@
 Changes
 =======
 
+Version v0.6.0 (release 2022-08-01)
+
+- make the jsonschema less restrictive
+
+
 Version v0.5.2 (release 2022-07-29)
 
 - use search react components provided by invenio-app-rdm
--- invenio_records_marc21/__init__.py
+++ invenio_records_marc21/__init__.py
@@ -211,7 +211,7 @@ from __future__ import absolute_import, print_function
 from .ext import InvenioRecordsMARC21
 from .proxies import current_records_marc21
 
-__version__ = "0.5.2"
+__version__ = "0.6.0"
 
 __all__ = (
     "__version__",
