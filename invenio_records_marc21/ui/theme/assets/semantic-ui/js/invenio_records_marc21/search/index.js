a626bac9e75abc5a35ecc415a6486665a99102a3
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-09-27 22:57:22 +0200

    fix: javascript dependencies

--- invenio_records_marc21/ui/theme/assets/semantic-ui/js/invenio_records_marc21/search/index.js
+++ invenio_records_marc21/ui/theme/assets/semantic-ui/js/invenio_records_marc21/search/index.js
@@ -5,28 +5,34 @@
 // Invenio-Records-Marc21 is free software; you can redistribute it and/or
 // modify it under the terms of the MIT License; see LICENSE file for more
 // details.
-
+import { parametrize } from "react-overridable";
 import { createSearchAppInit } from "@js/invenio_search_ui";
 import {
-  RDMBucketAggregationElement,
-  RDMRecordFacets,
-  RDMRecordFacetsValues,
   RDMRecordSearchBarContainer,
   RDMRecordMultipleSearchBarElement,
   RDMToggleComponent,
   RDMCountComponent,
 } from "@js/invenio_app_rdm/search/components";
 import {
+  ContribSearchAppFacets,
+  ContribBucketAggregationElement,
+  ContribBucketAggregationValuesElement,
+} from "@js/invenio_search_ui/components";
+import {
   Marc21RecordResultsGridItem,
   Marc21RecordResultsListItem,
 } from "./components";
 
+const ContribSearchAppFacetsWithConfig = parametrize(ContribSearchAppFacets, {
+  toogle: true,
+});
+
 const initSearchApp = createSearchAppInit({
-  "BucketAggregation.element": RDMBucketAggregationElement,
-  "BucketAggregationValues.element": RDMRecordFacetsValues,
+  "BucketAggregation.element": ContribBucketAggregationElement,
+  "BucketAggregationValues.element": ContribBucketAggregationValuesElement,
   "ResultsGrid.item": Marc21RecordResultsGridItem,
   "ResultsList.item": Marc21RecordResultsListItem,
-  "SearchApp.facets": RDMRecordFacets,
+  "SearchApp.facets": ContribSearchAppFacetsWithConfig,
   "SearchApp.searchbarContainer": RDMRecordSearchBarContainer,
   "SearchBar.element": RDMRecordMultipleSearchBarElement,
   "SearchFilters.ToggleComponent": RDMToggleComponent,
