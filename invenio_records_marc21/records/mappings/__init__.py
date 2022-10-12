0f8cc657184ff28bb6f01af7de2e699b3bff6dc1
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-10-12 09:15:48 +0200

    global: migrate to opensearch2

--- invenio_records_marc21/records/mappings/__init__.py
+++ invenio_records_marc21/records/mappings/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of Invenio.
 #
-# Copyright (C) 2021 Graz University of Technology.
+# Copyright (C) 2021-2022 Graz University of Technology.
 #
 # Invenio-Records-Marc21 is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -10,10 +10,8 @@
 
 """Mappings.
 
-Mappings define how records and their fields will be indexed in Elasticsearch.
+Mappings define how records and their fields will be indexed in Opensearch.
 The provided record-v1.0.0.json file is an example of how to index records
-in Elasticsearch. You need to provide one mapping per major version of
-Elasticsearch you want to support.
+in Opensearch. You need to provide one mapping per major version of
+Opensearch you want to support.
 """
-
-from __future__ import absolute_import, print_function
--- invenio_records_marc21/records/mappings/os-v2/__init__.py
+++ invenio_records_marc21/records/mappings/os-v2/__init__.py
@@ -2,12 +2,10 @@
 #
 # This file is part of Invenio.
 #
-# Copyright (C) 2021 Graz University of Technology.
+# Copyright (C) 2022 Graz University of Technology.
 #
 # Invenio-Records-Marc21 is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
 # details.
 
-"""Mappings for Elasticsearch 7.x."""
-
-from __future__ import absolute_import, print_function
+"""Mappings for Opensearch 2."""
