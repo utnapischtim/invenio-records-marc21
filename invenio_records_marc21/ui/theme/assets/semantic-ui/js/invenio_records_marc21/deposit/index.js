98183563ee570dd172017ebeae5ac9f975643e31
Christoph Ladurner <christoph.ladurner@tugraz.at>
2022-10-28 09:42:47 +0200

    fix: remove import of semantic css
    * it produces an error in the build process. error message
      'SassError: expected "{".'

--- invenio_records_marc21/ui/theme/assets/semantic-ui/js/invenio_records_marc21/deposit/index.js
+++ invenio_records_marc21/ui/theme/assets/semantic-ui/js/invenio_records_marc21/deposit/index.js
@@ -1,6 +1,6 @@
 // This file is part of Invenio.
 //
-// Copyright (C) 2021 Graz University of Technology.
+// Copyright (C) 2021-2022 Graz University of Technology.
 //
 // Invenio-Records-Marc21 is free software; you can redistribute it and/or
 // modify it under the terms of the MIT License; see LICENSE file for more
@@ -8,7 +8,6 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import "semantic-ui-css/semantic.min.css";
 
 import { getInputFromDOM } from "react-invenio-deposit";
 import { Marc21DepositForm } from "./Marc21DepositForm";
